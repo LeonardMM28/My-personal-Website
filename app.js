65de295d0581be878c5c67423edb4bb0b72bf889
Leonardo Meza Martinez <leonardomezamartinez@gmail.com>
2023-03-26 14:33:47 -0700

    lmm

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-var x = 232;
-console.log(x);
-
+function alert() {
+  alert(
+    "Hi there! \nI am Leonardo Meza \nand you have reached my BIO website \nneverthless I am still working on it \nhence I apologize if you \ncannot find too much info yet, \nyou might wanna come back \nanother time to check for updates. \nHave a nice day! :)"
+  );
+}
