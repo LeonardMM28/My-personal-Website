0c4729e76e0a3cd21d6da5bda5263ff5353eae48
Leonardo Meza Martinez <leonardomezamartinez@gmail.com>
2024-06-19 15:06:23 -0700

    chore: Remove unnecessary files and code

--- app.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", function (ev) {
-  alert(
-    "Hi there! I am Leonardo Meza and thanks for visiting my personal website, nevertheless I am still working on it, hence I apologize if you cannot find too much info yet, you might wanna come back another time to check for updates!! Have a nice day! :)"
-  );
-});
